c39423c17a3247f5fa79b7501f59b407f97fb96b
douglas.noah <douglas.noah7344@gmail.com>
2017-03-29 11:34:11 -0500

    Sammy's Motto and Carly's Motto

---
+++ Chapter1/src/CaseProblems/CarlysMotto2.java
@@ -0,0 +1,13 @@
+package CaseProblems;
+
+public class CarlysMotto2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("*********************************************");
+		System.out.println("Carly's Makes the food that makes it a party!");	
+		System.out.println("*********************************************");
+
+	}
+}
+
---
+++ Chapter1/src/CaseProblems/SammysMotto2.java
@@ -0,0 +1,12 @@
+package CaseProblems;
+
+public class SammysMotto2 {
+
+	public static void main(String[] args) {
+		System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+		System.out.println("Sammy's makes it fun in the sun.");
+		System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+
+	}
+
+}
