9ede2bebc16c0428ccc72a199a48229112234387
douglas.noah <douglas.noah7344@gmail.com>
2017-04-03 13:21:17 -0500

    End April 3

---
+++ Chapter2/src/Debugging/FixDebugTwo3.java
@@ -0,0 +1,10 @@
+package Debugging;
+
+public class FixDebugTwo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Chapter2/src/Debugging/FixDebugTwo4.java
@@ -0,0 +1,10 @@
+package Debugging;
+
+public class FixDebugTwo4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
