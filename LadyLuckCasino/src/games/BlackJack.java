25fb332f333f6b0a7eee6c0a30a4ee5831f11035
douglas.noah <douglas.noah7344@gmail.com>
2017-04-05 11:02:58 -0500

    Hawkeye Challenge 1

--- LadyLuckCasino/src/games/BlackJack.java
+++ LadyLuckCasino/src/games/BlackJack.java
@@ -1,5 +1,30 @@
 package games;
 
-public class BlackJack {
+import java.util.Random;
 
+import javax.swing.JOptionPane;
+
+public class BlackJack {
+	
+public static void main(String [] args){
+    Random rand = new Random();
+	
+	int cardOne = rand.nextInt(10)+1; 
+	int cardTwo = rand.nextInt(10)+1;
+	int cardTotal = cardOne + cardTwo;
+	
+	int result = JOptionPane.showConfirmDialog(null, "Card one: "+ cardOne + "\n Card two: "+ cardTwo
+		 + "\n Overall Card Total: "+ cardTotal + "\n Would you like to Hit?",null, JOptionPane.YES_NO_OPTION);
+	
+	if (result == JOptionPane.YES_OPTION){
+		boolean keepGoing = true;
+		while(keepGoing = true){
+		cardTotal += rand.nextInt(10)+1;
+		JOptionPane.showMessageDialog(null, cardTotal);
+		}
+	}
+	else{
+		JOptionPane.showMessageDialog(null, "Goodbye");
+	}
+  }
 }
